e18e22cd9f3c7da6f38517a4e4733a36548564e4
Joaoluislins <joaoluislins@gmail.com>
2022-04-12 14:08:45 -0300

    teste2

--- dags/twitter_to_s3.py
+++ dags/twitter_to_s3.py
@@ -1,7 +1,7 @@
 # DAG to ingest user's timeline from twitter to am aws s3 instance.
 
 # Importing modules and libraries
-import airflowa
+import airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
 from datetime import datetime, timedelta
