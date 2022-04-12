e676f7aad272307c12e99b86670499fb5ee0fe03
Joaoluislins <joaoluislins@gmail.com>
2022-04-12 17:46:50 -0300

    sas

--- dags/twitter/data_writer.py
+++ dags/twitter/data_writer.py
@@ -12,17 +12,17 @@ class DataWriter:
 
   def _write_row(self, data: dict) -> None:
     #os.makedirs(os.path.dirname(self.filename), exist_ok = True)
-    print(os.getcwd())
-    os.chdir('/opt/')
+    #print(os.getcwd())
+    #os.chdir('/opt/***')
     print(os.getcwd())
     os.chdir('outputs/')
     print(os.getcwd())
     if not os.path.isdir(f"timelines/{self.user_id}/"):
       os.makedirs(os.path.dirname(f"timelines/{self.user_id}/"), exist_ok = True)
-    path = f"timelines/{self.user_id}/"
+    #path = f"timelines/{self.user_id}/"
     os.chdir(path)
     print(os.getcwd())
-    with open(self.filename, 'a') as f:
+    with open(f"timelines/{self.user_id}/{self.filename}", 'a') as f:
       for item in data['data']:
         #print(type(item))
         #print(item)
