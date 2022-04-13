b0288358853006c4558ba44b634ec4897a1c8167
Joaoluislins <joaoluislins@gmail.com>
2022-04-13 16:07:37 -0300

    change to aws env

--- dags/aws/bucket.py
+++ dags/aws/bucket.py
@@ -12,7 +12,7 @@ from datetime import datetime
 logger = logging.getLogger(__name__)
 logging.basicConfig(level = logging.INFO)
 
-load_dotenv('/opt/airflow/outputs/.env')
+load_dotenv('/opt/airflow/aws_ev/.env')
 
 class Bucket(ABC):
     def __init__(self, bucket_name) -> None:
